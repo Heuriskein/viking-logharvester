2d239c5a9ab567691cae2282001a771f4bd02fcd
hrakarth@gmail.com <hrakarth@gmail.com>
2014-12-24 14:53:50 -0800

    Changed script to work with >1 core/log per server

--- harvester.py
+++ harvester.py
@@ -16,27 +16,28 @@ my_id = socket.gethostbyname(socket.gethostname())
 
 
 for d, dirs, files in os.walk('/opt/vikinggameserver/logs/'): #   development/port/log
-    log = None
-    core = None
+    log = []
+    core = []
     for f in files:
         if f.endswith('.core'):
-            core = f
+            core.append(f)
 
         if f.endswith('.log'):
-            log = f
+            log.append(f)
 
-    if log is not None:
-        remainder, port = os.path.split(d)
-        remainder, branch = os.path.split(remainder)
-        now = str(datetime.datetime.utcnow())
+    remainder, port = os.path.split(d)
+    remainder, branch = os.path.split(remainder)
+    now = str(datetime.datetime.utcnow())
+    core_switch = 'nocrash'
+    if len(core) > 0:
+        core_switch = 'crash'
 
-        core_switch = 'nocrash'
-        if core is not None:
-            core_switch = 'crash'
-            corekeyname = posixpath.join('logs', branch, core_switch, my_id, port, 'core')
-            corekey = bucket.new_key(corekeyname)
-            corekey.set_contents_from_filename(os.path.join(d, core))
-
-        logkeyname = posixpath.join('logs', branch, core_switch, my_id, port, now, 'log')
+    for log_i in xrange(len(log)):
+        logkeyname = posixpath.join('logs', branch, core_switch, my_id, port, now, 'log-{}'.format(log_i))
         logkey = bucket.new_key(logkeyname)
-        logkey.set_contents_from_filename(os.path.join(d, log))
+        logkey.set_contents_from_filename(os.path.join(d, log[log_i]))
+
+    for core_i in xrange(len(core)):
+        corekeyname = posixpath.join('logs', branch, core_switch, my_id, port, now, 'core-{}'.format(core_i))
+        corekey = bucket.new_key(corekeyname)
+        corekey.set_contents_from_filename(os.path.join(d, core[core_i]))
