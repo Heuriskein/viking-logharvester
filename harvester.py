d042c6eb9ae8c698029fc8a4f487334b256d15e3
hrakarth@gmail.com <hrakarth@gmail.com>
2014-12-24 14:55:54 -0800

    Changed the order of directories to better match the expected use-case

--- harvester.py
+++ harvester.py
@@ -13,7 +13,7 @@ bucket = connection.get_bucket('vikinggame-logs')
 
 # This is imperfect, since IPs will be reused. but hopefully the timestamp combined with the IP is sufficient
 my_id = socket.gethostbyname(socket.gethostname())
-
+now = str(datetime.datetime.utcnow())
 
 for d, dirs, files in os.walk('/opt/vikinggameserver/logs/'): #   development/port/log
     log = []
@@ -27,17 +27,16 @@ for d, dirs, files in os.walk('/opt/vikinggameserver/logs/'): #   development/po
 
     remainder, port = os.path.split(d)
     remainder, branch = os.path.split(remainder)
-    now = str(datetime.datetime.utcnow())
     core_switch = 'nocrash'
     if len(core) > 0:
         core_switch = 'crash'
 
     for log_i in xrange(len(log)):
-        logkeyname = posixpath.join('logs', branch, core_switch, my_id, port, now, 'log-{}'.format(log_i))
+        logkeyname = posixpath.join('logs', branch, core_switch, now, my_id, port, 'log-{}'.format(log_i))
         logkey = bucket.new_key(logkeyname)
         logkey.set_contents_from_filename(os.path.join(d, log[log_i]))
 
     for core_i in xrange(len(core)):
-        corekeyname = posixpath.join('logs', branch, core_switch, my_id, port, now, 'core-{}'.format(core_i))
+        corekeyname = posixpath.join('logs', branch, core_switch, now, my_id, port, 'core-{}'.format(core_i))
         corekey = bucket.new_key(corekeyname)
         corekey.set_contents_from_filename(os.path.join(d, core[core_i]))
